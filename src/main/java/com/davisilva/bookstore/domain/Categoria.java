d77ed39a7eae55fc682e974a5aee36a62cc35f51
Davi Silva <davi.brito7226@gmail.com>
2022-07-06 13:04:06 -0300

    finalizando classes de dominio

--- src/main/java/com/davisilva/bookstore/domain/Categoria.java
+++ src/main/java/com/davisilva/bookstore/domain/Categoria.java
@@ -1,12 +1,94 @@
 package com.davisilva.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
+@Entity /*Vai informar ao JPA que a classe categoria é uma entidade e pode criar uma tabela na base de dados*/
+public class Categoria implements Serializable {
+	/**
+	Habilita que o objeto da classe  possa ter seu estado persistido na api padrao do java
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id /*informa que o id é uma chave primaria*/
+	@GeneratedValue(strategy = GenerationType.IDENTITY) /*Valor da chave primaria sera gerar pelo auto incremetendo da tabela categogia*/
 	private Integer id;
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria") /*Uma categoria para uma lista de livros*/
 	private List<Livro> livros = new ArrayList<>();
+
+	public Categoria() {
+		super();
+	}
+
+	public Categoria(Integer id, String nome, String descricao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(List<Livro> livros) {
+		this.livros = livros;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
 }
--- src/main/java/com/davisilva/bookstore/domain/Livro.java
+++ src/main/java/com/davisilva/bookstore/domain/Livro.java
@@ -1,13 +1,32 @@
 package com.davisilva.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ManyToAny;
+
+@Entity
+public class Livro implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nome_autor;
 	private String texto;
 
+	@ManyToOne /*Muitos livros para uma categoria */
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
