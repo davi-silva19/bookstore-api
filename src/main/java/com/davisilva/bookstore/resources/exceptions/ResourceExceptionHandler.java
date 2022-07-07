f69c65ee142391b124cd8d33fd2fdab023befd52
Davi Silva <davi.brito7226@gmail.com>
2022-07-07 12:43:02 -0300

    Exceção personalizada para Objeto não encontrado

---
+++ src/main/java/com/davisilva/bookstore/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.davisilva.bookstore.resources.exceptions;
+
+import javax.servlet.ServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.davisilva.bookstore.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandarError> objectNotFoundException(ObjectNotFoundException e, ServletRequest request) {
+		StandarError error = new StandarError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+}
--- src/main/java/com/davisilva/bookstore/services/CategoriaService.java
+++ src/main/java/com/davisilva/bookstore/services/CategoriaService.java
@@ -7,14 +7,16 @@ import org.springframework.stereotype.Service;
 
 import com.davisilva.bookstore.domain.Categoria;
 import com.davisilva.bookstore.repositories.CategoriaRepository;
+import com.davisilva.bookstore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
